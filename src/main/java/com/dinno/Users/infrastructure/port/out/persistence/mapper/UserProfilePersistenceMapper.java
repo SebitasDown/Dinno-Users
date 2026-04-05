43e1238c8ce6d318163c605d92c34673a2ec1a0b
SebitasDown <areizasebas31@gmail.com>
2026-04-05 15:37:29 -0500

    fix

--- src/main/java/com/dinno/Users/infrastructure/port/out/persistence/mapper/UserProfilePersistenceMapper.java
+++ src/main/java/com/dinno/Users/infrastructure/port/out/persistence/mapper/UserProfilePersistenceMapper.java
@@ -3,6 +3,7 @@ package com.dinno.Users.infrastructure.port.out.persistence.mapper;
 import com.dinno.Users.domain.model.UserProfile;
 import com.dinno.Users.infrastructure.port.out.persistence.entity.UserProfileEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
@@ -10,6 +11,7 @@ public interface UserProfilePersistenceMapper {
 
     UserProfilePersistenceMapper INSTANCE = Mappers.getMapper(UserProfilePersistenceMapper.class);
 
+    @Mapping(target = "isNewProfile", ignore = true)
     UserProfileEntity toEntity(UserProfile model);
 
     UserProfile toModel(UserProfileEntity entity);
